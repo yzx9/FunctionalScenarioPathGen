5dfa282bb87c9324dd22f6e8869731f0fb585ffe
Celeste <yuan.zx@outlook.com>
2022-05-22 22:52:55 +0800

    feat: gen paths

---
+++ src/__tests__/pathGen.ts
@@ -0,0 +1,58 @@
+import { divide, getDifference, getFullArray } from "../pathGen"
+
+test("get full array", () => {
+  expect(getFullArray(1, 1)).toEqual([[0]])
+  expect(getFullArray(2, 1)).toEqual([[0], [1]])
+  expect(getFullArray(2, 2)).toEqual([[0, 1]])
+  expect(getFullArray(3, 1)).toEqual([[0], [1], [2]])
+  expect(getFullArray(3, 2)).toEqual([
+    [0, 1],
+    [0, 2],
+    [1, 2],
+  ])
+  expect(getFullArray(3, 3)).toEqual([[0, 1, 2]])
+  expect(getFullArray(4, 1)).toEqual([[0], [1], [2], [3]])
+  expect(getFullArray(4, 2)).toEqual([
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 2],
+    [1, 3],
+    [2, 3],
+  ])
+  expect(getFullArray(4, 3)).toEqual([
+    [0, 1, 2],
+    [0, 1, 3],
+    [0, 2, 3],
+    [1, 2, 3],
+  ])
+  expect(getFullArray(4, 4)).toEqual([[0, 1, 2, 3]])
+})
+
+test("get difference", () => {
+  expect(getDifference(1, [])).toEqual([0])
+  expect(getDifference(2, [])).toEqual([0, 1])
+  expect(getDifference(3, [])).toEqual([0, 1, 2])
+
+  expect(getDifference(3, [0])).toEqual([1, 2])
+  expect(getDifference(3, [1])).toEqual([0, 2])
+  expect(getDifference(3, [0, 1])).toEqual([2])
+  expect(getDifference(3, [0, 2])).toEqual([1])
+  expect(getDifference(3, [1, 2])).toEqual([0])
+  expect(getDifference(3, [0, 1, 2])).toEqual([])
+
+  expect(getDifference(4, [0])).toEqual([1, 2, 3])
+  expect(getDifference(4, [0, 2])).toEqual([1, 3])
+  expect(getDifference(4, [0, 1, 2, 3])).toEqual([])
+})
+
+test("divide array", () => {
+  expect(divide(["a", "b"], [])).toEqual([[], ["a", "b"]])
+  expect(divide(["a", "b"], [0])).toEqual([["a"], ["b"]])
+  expect(divide(["a", "b"], [1])).toEqual([["b"], ["a"]])
+  expect(divide(["a", "b"], [0, 1])).toEqual([["a", "b"], []])
+
+  expect(divide(["a", "b", "c"], [0])).toEqual([["a"], ["b", "c"]])
+  expect(divide(["a", "b", "c"], [0, 1])).toEqual([["a", "b"], ["c"]])
+  expect(divide(["a", "b", "c"], [0, 1, 2])).toEqual([["a", "b", "c"], []])
+})
--- src/pathGen.ts
+++ src/pathGen.ts
@@ -4,6 +4,7 @@ import { Type, merge, isSubSet } from "./type"
 export class Path {
   processes: Process[]
   adjacencyMatrix: boolean[][]
+  layers: Process[][]
 
   constructor(processes: Process[]) {
     this.processes = processes
@@ -12,24 +13,130 @@ export class Path {
         .fill(0)
         .map((_) => [...Array(processes.length).fill(false)]),
     ]
+    this.layers = []
+  }
+
+  copy(): Path {
+    const path = new Path(this.processes)
+    path.adjacencyMatrix = this.adjacencyMatrix.map((a) => a.map((b) => b))
+    path.layers = this.layers.map((a) => a.map((b) => b))
+    return path
   }
 }
 
 export function genFuntionalScenarioPaths(processes: Process[]): Path[] {
-  const paths = []
+  const paths: Path[] = []
 
-  for (let i = 0; i < processes.length; i++) {
-    let inputs: Type[][] = []
-    let input = merge(...inputs)
+  for (let i = 1; i <= processes.length; i++) {
+    const fullArray = getFullArray(processes.length, i)
 
-    let output: Type[] = []
-    if (!isSubSet(output, input)) {
-      // this is invalid path, notes that this is not about validity
+    for (let notUsedIndexs of fullArray) {
+      const [notUsed, used] = divide(processes, notUsedIndexs)
+
+      const path = new Path(processes)
+      path.layers.push(used)
+
+      // let inputs = merge(...pro.map((a) => a.x))
+      const outputs = merge(...used.map((a) => a.y))
+      const subpaths = genPathLayer(path, outputs, notUsed)
+      paths.push(...subpaths)
     }
-
-    // put valid path
-    // gen next level nodes
   }
 
   return paths
+}
+
+/**
+ *
+ * @param lastPath 上一级的Path
+ * @param lastOutputs 上一级的输出
+ * @param processes 可用的Process
+ * @returns
+ */
+function genPathLayer(
+  lastPath: Path,
+  lastOutputs: Type[],
+  processes: Process[]
+): Path[] {
+  const paths = []
+  for (let i = 0; i < processes.length; i++) {
+    let fullArray = getFullArray(processes.length, i)
+
+    for (let notUsedIndexs of fullArray) {
+      const [notUsed, used] = divide(processes, notUsedIndexs)
+      const inputs = merge(...used.map((a) => a.x))
+      if (!isSubSet(inputs, lastOutputs)) {
+        // this is invalid path, notes that this is not about validity
+        continue
+      }
+
+      const path = lastPath.copy()
+      path.layers.push(used)
+
+      const outputs = merge(...used.map((a) => a.y))
+      // TODO: update path
+
+      const subpaths = genPathLayer(path, outputs, notUsed)
+      paths.push(...subpaths)
+    }
+  }
+
+  return paths
+}
+
+export function getFullArray(
+  n: number,
+  k: number,
+  offset: number = 0
+): number[][] {
+  if (k == 0) return [[]]
+  if (n <= 0) return []
+
+  const array: number[][] = []
+  for (let i = offset; i < n - k + 1; i++) {
+    const arr = getFullArray(n, k - 1, i + 1)
+    array.push(...arr.map((a) => [i, ...a]))
+  }
+
+  return array
+}
+
+/**
+ * 求差集
+ * @param n set [0..n-1]
+ * @param set ordered set
+ */
+export function getDifference(n: number, set: number[]): number[] {
+  const arr: number[] = []
+  let j = 0
+  for (let i = 0; i < n; i++) {
+    if (j == set.length || i < set[j]) {
+      arr.push(i)
+    } else {
+      j++
+    }
+  }
+  return arr
+}
+
+/**
+ * 将数组依照Indexs拆分为两个数组
+ * @param arr
+ * @param indexs order array index
+ * @returns
+ */
+export function divide<T>(arr: T[], indexs: number[]): [T[], T[]] {
+  const re: [T[], T[]] = [[], []]
+
+  let j = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (j === indexs.length || i < indexs[j]) {
+      re[1].push(arr[i])
+    } else {
+      re[0].push(arr[i])
+      j++
+    }
+  }
+
+  return re
 }
