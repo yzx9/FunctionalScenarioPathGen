3c54e961bc07e3726942955e218eda19bbb3495a
HuangZheng <huangzheng@shu.edu.cn>
2022-05-23 19:40:38 +0800

    refactor: rewrite test case

--- src/__tests__/NumberAxis.ts
+++ src/__tests__/NumberAxis.ts
@@ -3,22 +3,34 @@ import { Condition, Predicate, Relation } from "../condition"
 
 test("NumberAxis eq", () => {
   const axis = new NumberAxis().add(Predicate.eq, 1).add(Predicate.eq, 2)
-  expect(axis.intervalSpaces.length).toBe(2)
+  expect(axis.toString()).toBe("[1,1],[2,2]")
 })
 
-test("NumberAxis lt", () => {
-  const axis = new NumberAxis().add(Predicate.lt, 0)
-  expect(axis.add(Predicate.lt, -1).intervalSpaces.length).toBe(1)
+test("NumberAxis (-Infinity,0)", () => {
+  const axis = new NumberAxis().add(Predicate.lt, 0).add(Predicate.lt, -1)
+  expect(axis.toString()).toBe("(-Infinity,0)")
 })
-test("NumberAxis lt2", () => {
+test("NumberAxis (-Infinity,0)", () => {
   const axis = new NumberAxis().add(Predicate.lt, -1).add(Predicate.lt, 0)
-  expect(axis.intervalSpaces.length).toBe(1)
+  expect(axis.toString()).toBe("(-Infinity,0)")
 })
-test("NumberAxis 32", () => {
+test("NumberAxis (-Infinity,Infinity)", () => {
   const axis = new NumberAxis().add(Predicate.lt, -1).add(Predicate.qe, -1)
-  expect(axis.intervalSpaces.length).toBe(1)
+  expect(axis.toString()).toBe("(-Infinity,Infinity)")
 })
-test("NumberAxis 32", () => {
+test("NumberAxis [-1,-1]", () => {
   const axis = new NumberAxis().add(Predicate.eq, -1).add(Predicate.eq, -1)
-  expect(axis.intervalSpaces.length).toBe(1)
+  expect(axis.toString()).toBe("[-1,-1]")
 })
+
+// test("NumberAxis 32", () => {
+//   const axis = new NumberAxis()
+//   axis.add(Predicate.le, 0).add(Predicate.qe, -1)
+//   expect(axis.toString()).toBe("[-1,0]")
+//   axis.add(Predicate.le, 1)
+//   expect(axis.toString()).toBe("[-Infinity,1]")
+//   axis.add(Predicate.qe, 2)
+//   expect(axis.toString()).toBe("[-Infinity,1],[2,Infinity]")
+//   axis.add(Predicate.lt, 3)
+//   expect(axis.toString()).toBe("[-Infinity,1],[2,3)")
+// })
